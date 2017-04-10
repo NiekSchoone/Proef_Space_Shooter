85aac4aca99b74f1169c487c3b14a9aee7a8c1ac
Collix <15986@ma-web.nl>
2017-04-10 14:53:03 +0200

    Now lerping the movement

---
+++ Game_Space_Shooter/src/Ships/Player/Player.ts
@@ -0,0 +1,32 @@
+﻿/// <reference path="../Ship.ts"/>
+class Player extends Ship
+{
+    private mouseDown: boolean;
+
+    constructor(game: Phaser.Game)
+    {
+        super(game);
+
+        this.loadTexture("tempship");
+        this.game.add.existing(this);
+        this.speed = 10;
+
+        this.scale.set(0.25);
+        this.anchor.set(0.5);
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.arcade.enable(this);
+    }
+
+    update()
+    {
+        if (this.game.input.mousePointer.isDown)
+        {
+            var dirx = (this.game.input.x - this.x) / 100;
+            var diry = (this.game.input.y - this.y) / 100;
+
+            this.x += dirx * this.speed;
+            this.y += diry * this.speed;
+        }
+    }
+                                  
+}
