918739bcfb71d384517921f9472c4f17c1b92d4f
BeauArthurs <B.ArthursGames@gmail.com>
2017-04-10 15:15:59 +0200

    -added enemy -added movementpattern

---
+++ Game_Space_Shooter/src/Enemies/Enemy.ts
@@ -0,0 +1,23 @@
+﻿enum EnemyType
+{
+}
+class Enemy //extends Ship
+{
+    private direction: Vector2;
+    private enemyType: EnemyType;
+
+    //constructor(_healthMod: number, _speedMod: number)
+   // {
+
+    //}
+
+    public OnHit(_amount: number)
+    {
+        //this.health -= _amount;
+    }
+    private Destroy()
+    {
+        
+    }
+
+}
